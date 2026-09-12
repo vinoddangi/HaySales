0cd13eb99220f7a2729e82fd8ddd48488abba790
Vinod Dangi <vinoddangi@gmail.com>
2026-09-12 23:49:30 +0530

    fix some issue

--- src/types.ts
+++
@@ -1,6 +0,0 @@
-export interface Customer {
-  id: string; // Document ID
-  Id: number; // Numeric sequence ID
-  Name: string; // Customer name
-  totalOutstandingDue?: number; // Added to support tracking dues!
-}
--- src/types/index.ts
+++ src/types/index.ts
@@ -33,3 +33,10 @@ export interface SnackbarState {
   actionLabel?: string;
   onAction?: () => void;
 }
+
+export interface Customer {
+  id: string; // Document ID
+  Id: number; // Numeric sequence ID
+  Name: string; // Customer name
+  totalOutstandingDue?: number; // Added to support tracking dues!
+}
